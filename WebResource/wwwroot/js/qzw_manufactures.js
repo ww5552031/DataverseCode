b52bb4a675bc111edecc74fe8b0d47e82a522bfa
qzwang <qzwang@ASUS-ZEN-QZ>
2026-03-22 19:56:15 +0800

    js

---
+++ WebResource/wwwroot/js/qzw_manufactures.js
@@ -0,0 +1,24 @@
+﻿window.manufacturesform = window.manufacturesform || {};
+
+window.manufacturesform.load = function (executionContext) {
+    let formContext = executionContext.getFormContext();
+    
+    formContext.getAttribute("qzw_titleen").addOnChange(manufacturesform.titleen_onChange);
+}
+
+
+window.manufacturesform.titleen_onChange = function (executionContext) {
+
+    debugger;
+    let formContext = executionContext.getFormContext();
+    let eventSource = executionContext.getEventSource();
+    let fieldVal = eventSource.getValue();
+    let fieldControl = eventSource.controls.get(0);
+    if (fieldVal) {
+        fieldControl.clearNotification("phoneWarning");
+    } else {
+        fieldControl.setNotification("哈哈哈", "phoneWarning");
+    }
+
+}
+
